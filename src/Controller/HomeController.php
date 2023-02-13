46e0bf53464cdbb357788024a1c3843554b1974b
Ben Sales <adebsa2401@gmail.com>
2023-02-13 10:25:43 +0100

    fix: Change dashboard routes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,39 +9,21 @@ use Symfony\Component\Security\Core\Security;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/dashboard', name: 'app_home')]
     public function home(Security $security): Response
     {
         if ($security->isGranted('ROLE_ADMIN')) {
-            return $this->redirectToRoute('admin_dashboard');
+            return $this->render('admin_dashboard.html.twig');
         }
 
         if ($security->isGranted('ROLE_INDIVIDUAL')) {
-            return $this->redirectToRoute('individual_dashboard');
+            return $this->render('individual_dashboard.html.twig');
         }
 
         if ($security->isGranted('ROLE_COMPANY')) {
-            return $this->redirectToRoute('company_dashboard');
+            return $this->render('company_dashboard.html.twig');
         }
         
         return $this->redirectToRoute('app_login');
-    }
-
-    #[Route('/admin/dashboard', name: 'admin_dashboard')]
-    public function adminDashboard(): Response
-    {
-        return $this->render('admin_dashboard.html.twig');
-    }
-
-    #[Route('/individual/dashboard', name: 'individual_dashboard')]
-    public function individualDashboard(): Response
-    {
-        return $this->render('individual_dashboard.html.twig');
-    }
-
-    #[Route('/company/dashboard', name: 'company_dashboard')]
-    public function companyDashboard(): Response
-    {
-        return $this->render('company_dashboard.html.twig');
     }
 }
