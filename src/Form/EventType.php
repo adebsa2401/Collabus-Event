5bcf48d5a15a74ce359e522c4dad1b4f38107c7a
Ben Sales <adebsa2401@gmail.com>
2023-02-14 01:34:39 +0100

    feat: Create Event crud templates

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -3,7 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Event;
+use App\Entity\EventType as EntityEventType;
+use App\Entity\Place;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +17,43 @@ class EventType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('startedAt')
-            ->add('endedAt')
-            ->add('type')
-            ->add('place')
+            ->add('name', TextType::class, [
+                'label' => 'Name',
+                'attr' => [
+                    'placeholder' => 'Name',
+                ],
+            ])
+            ->add('startedAt', DateType::class, [
+                'label' => 'Commence le',
+                'widget' => 'single_text',
+                'attr' => [
+                    'placeholder' => 'Commence le',
+                ],
+            ])
+            ->add('endedAt', DateType::class, [
+                'label' => 'Fini le',
+                'widget' => 'single_text',
+                'attr' => [
+                    'placeholder' => 'Fini le',
+                ],
+            ])
+            ->add('type', EntityType::class, [
+                'class' => EntityEventType::class,
+                'choice_label' => 'name',
+                'label' => 'Type',
+                'attr' => [
+                    'placeholder' => 'Choisir le type',
+                ],
+            ])
+            ->add('place', EntityType::class, [
+                'class' => Place::class,
+                'choice_label' => 'name',
+                'label' => 'Lieu',
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Choisir le lieu',
+                ],
+            ])
         ;
     }
 
--- src/Form/EventTypeType.php
+++ src/Form/EventTypeType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\EventType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,12 @@ class EventTypeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'label' => 'Name',
+                'attr' => [
+                    'placeholder' => 'Name',
+                ],
+            ])
         ;
     }
 
