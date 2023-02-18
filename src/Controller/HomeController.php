de9f40b93124a980109ff183c56d8b1e9c214d05
Ben Sales <adebsa2401@gmail.com>
2023-02-18 17:03:51 +0100

    feat: Integrate template in auth base

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,26 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Security;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function home(Security $security): Response
+    public function home(): Response
     {
-        if ($security->isGranted('ROLE_ADMIN')) {
-            return $this->render('admin_dashboard.html.twig');
-        }
-
-        if ($security->isGranted('ROLE_INDIVIDUAL')) {
-            return $this->render('individual_dashboard.html.twig');
-        }
-
-        if ($security->isGranted('ROLE_COMPANY')) {
-            return $this->render('company_dashboard.html.twig');
-        }
-        
-        return $this->redirectToRoute('app_login');
+        return $this->render('dashboard.html.twig');
     }
 }
