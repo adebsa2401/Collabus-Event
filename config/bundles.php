f7141ce1a28bc9d13db03af574064a08aa759120
Ben Sales <adebsa2401@gmail.com>
2023-02-22 14:45:09 +0100

    fix: remove qr code packages

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,4 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
-    Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
 ];
