fbe7d4c73717a0979ab4702a6ba4d2649d365b6e
Ben Sales <adebsa2401@gmail.com>
2023-03-08 06:54:50 +0100

    feat: Add events and companies details page on home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Company;
+use App\Entity\Event;
 use App\Entity\EventParticipationRequest;
 use App\Entity\EventType;
 use App\Repository\ActivityAreaRepository;
 use App\Repository\CompanyProfileRepository;
 use App\Repository\CompanyRepository;
+use App\Repository\EventAttendanceRepository;
 use App\Repository\EventParticipationRequestRepository;
 use App\Repository\EventRepository;
 use App\Repository\EventTypeRepository;
@@ -18,7 +21,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'app_home', methods: ['GET'])]
     public function home(
         EventRepository $eventRepository,
         CompanyRepository $companyRepository,
@@ -76,11 +79,73 @@ class HomeController extends AbstractController
         ));
     }
 
-    #[Route('/public/event-type/{id}', name: 'app_home_event_type_show')]
+    #[Route('/public/event-type/{id}', name: 'app_home_event_type_show', methods: ['GET'])]
     public function eventTypeShow(EventType $eventType, EventRepository $eventRepository): Response
     {
         $events = $eventRepository->findUpcomingEvents();
         return $this->render('home/event_type/show.html.twig', compact('eventType', 'events'));
+    }
+
+    #[Route('/public/event/{id}', name: 'app_home_event_show', methods: ['GET'])]
+    public function eventShow(Event $event, EventParticipationRequestRepository $eventParticipationRequestRepository, CompanyRepository $companyRepository, IndividualProfileRepository $individualProfileRepository): Response
+    {
+        // Calculate global attendance rate
+        $globalAttendanceRate = $event->getAttendanceRate();
+
+        // Calculate request approvement rate
+        $totalRequests = $event->getParticipationRequests()->count();
+        $totalParticipations = $event->getAttendances()->count();
+        $globalAttendanceValidationRate = $totalRequests > 0 ? $totalParticipations / $totalRequests : 0;
+
+        // calculate companies interest rate
+        $totalRequestsByCompany = count(array_filter($eventParticipationRequestRepository->findBy(['event' => $event]), fn (EventParticipationRequest $request) => $request->getCreatedBy()->getCompanyProfile()));
+        $totalCompany = count($companyRepository->findAll());
+        $companiesInterestRate = $totalCompany > 0 ? $totalRequestsByCompany / $totalCompany : 0;
+
+        // calculate individual profiles interest rate
+        $totalRequestsByIndividual = count(array_filter($eventParticipationRequestRepository->findBy(['event' => $event]), fn (EventParticipationRequest $request) => $request->getCreatedBy()->getIndividualProfile()));
+        $totalIndividual = count($individualProfileRepository->findAll());
+        $individualsInterestRate = $totalIndividual > 0 ? $totalRequestsByIndividual / $totalIndividual : 0;
+
+        return $this->render('home/event/show.html.twig', compact(
+            'event',
+            'globalAttendanceRate',
+            'globalAttendanceValidationRate',
+            'companiesInterestRate',
+            'individualsInterestRate',
+        ));
+    }
+
+    #[Route('/public/company/{id}', name: 'app_home_company_show', methods: ['GET'])]
+    public function companyShow(Company $company, EventParticipationRequestRepository $eventParticipationRequestRepository, EventRepository $eventRepository, EventAttendanceRepository $eventAttendanceRepository): Response
+    {
+        $participationRequests = $eventParticipationRequestRepository->findBy(['createdBy' => $company->getOwner()->getUser()]);
+        $participationRequestsCount = count($participationRequests);
+        $participatedEvents = array_filter($participationRequests, fn (EventParticipationRequest $request) => $request->getStatus() === EventParticipationRequest::STATUS_ACCEPTED);
+        $participatedEventsCount = count(array_unique(array_map(fn (EventParticipationRequest $request) => $request->getEvent()->getName(), $participatedEvents)));
+
+        $eventsCount = count($eventRepository->findAll());
+        $interestRate = $eventsCount > 0 ? $participationRequestsCount / $eventsCount : 0;
+        $interestRate = min($interestRate, 1);
+
+        $attendancesCount = count($eventAttendanceRepository->findBy(['representedCompany' => $company]));
+        $verifiedAttendancesCount = count($eventAttendanceRepository->findBy(['representedCompany' => $company, 'isVerified' => true]));
+        $attendanceRate = $attendancesCount > 0 ? $verifiedAttendancesCount / $attendancesCount : 0;
+        $attendanceRate = min($attendanceRate, 1);
+
+        $approvedParticipationRequests = array_filter($participationRequests, fn (EventParticipationRequest $request) => $request->getStatus() === EventParticipationRequest::STATUS_ACCEPTED);
+        $approvedParticipationRequestsCount = count($approvedParticipationRequests);
+        $participationRate = $participationRequestsCount > 0 ? $approvedParticipationRequestsCount / $participationRequestsCount : 0;
+        $participationRate = min($participationRate, 1);
+
+        return $this->render('home/company/show.html.twig', compact(
+            'company',
+            'participatedEventsCount',
+            'participationRequestsCount',
+            'interestRate',
+            'attendanceRate',
+            'participationRate'
+        ));
     }
 
     #[Route('/dashboard', name: 'app_dashboard')]
